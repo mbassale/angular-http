086e1bb0b03c60e8b0405af2a59d3511bf62d06d
Marco Bassaletti <bassaletti@gmail.com>
2017-10-02 08:42:49 -0300

    Created youtube search component with angular/http and RxJS.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import {AppComponent} from './app.component';
+import {SimpleHttpComponent} from './simple-http/simple-http.component';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {YoutubeSearchComponent} from './youtube-search/youtube-search.component';
+import {SearchBoxComponent} from './youtube-search/search-box.component';
+import {youTubeSearchInjectables} from './youtube-search/you-tube-search.injectables';
+import {SearchResultComponent} from './youtube-search/search-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleHttpComponent
+    SimpleHttpComponent,
+    YoutubeSearchComponent,
+    SearchBoxComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    youTubeSearchInjectables
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
